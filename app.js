67e45ae83d7f3cf389517248ba94c23d59b4d2fb
Vinicius <vinicius.cscience@gmail.com>
2016-07-20 16:00:01 -0300

    Ok

--- app.js
+++ app.js
@@ -1,60 +1,7 @@
 var memory = angular.module("memory", []);
 
 memory.controller("mainCtrl", function($scope, $timeout, $window){
-	$scope.gamesecond = 0;
-	$scope.gameminute= 0;
-
-	$scope.choose = [{line: -1, colum: -1},{line: -1, colum: -1}];
-	$scope.gameWin = false;
-	
-
-	$scope.game = [['A','D','C','T'],['T','E','D','B'],['A','M','C','E'],['F','B','M','F']];
-	$scope.gameTurned = [[false,false,false],[false,false,false],[false,false,false],[false,false,false]];
-
-	$scope.gameFull = ($scope.game.length * $scope.game[0].length)/2;
-
-	$scope.gameRight = 0;
-	$scope.gameWrong = 0;
-
-	$scope.setOption = function(line,colum){
-
-		if($scope.choose[0].line == -1){
-			$scope.choose[0].line = line;
-			$scope.choose[0].colum = colum;
-
-			$scope.gameTurned[$scope.choose[0].line][$scope.choose[0].colum] = true;
-			$scope.choose[1].line = -1;
-			$scope.choose[1].colum = -1;
-		}else if($scope.choose[1].line == -1 && !$scope.gameTurned[line][colum]){
-
-			$scope.choose[1].line = line;
-			$scope.choose[1].colum = colum;
-			$scope.gameTurned[$scope.choose[1].line][$scope.choose[1].colum] = true;
-			$timeout(function() {
-			var aux = [{line: $scope.choose[0].line, colum: $scope.choose[0].colum},{line: $scope.choose[1].line, colum: $scope.choose[1].colum}];
-
-					if($scope.game[$scope.choose[0].line][$scope.choose[0].colum] != $scope.game[$scope.choose[1].line][$scope.choose[1].colum]){
-						$scope.gameTurned[aux[0].line][aux[0].colum] = false;
-						$scope.gameTurned[aux[1].line][aux[1].colum] = false;
-
-						$scope.gameWrong++;
-				
-					}else{
-						$scope.gameRight++;
-						if($scope.gameFull == $scope.gameRight){
-							$scope.gameWin = true;
-							$timeout.cancel($scope.timeProm);
-						}
-
-					}
-					$scope.choose[0].line = -1;
-					$scope.choose[0].colum = -1;
-
-				}, 500);
-			
-		}
-
-	}
+	$scope.currentLevel = 1;
 
 	$scope.initCounter = function(){
 		$scope.timeProm = $timeout(function() {
@@ -62,19 +9,98 @@ memory.controller("mainCtrl", function($scope, $timeout, $window){
 			if($scope.gamesecond == 0){
 				$scope.gameminute++;
 			}
-
-			
-			
 			$scope.initCounter();
 		}, 1000);
-
 	}
-	$scope.initCounter();
+
+
+	$scope.initialize = function(m,n){
+		if(m%2 !=0 || n%2!=0) throw "A matriz precisa ter uma quantidade par de elementos";
+		$scope.gameFull = (m*n)/2;
+		$scope.gameWin = false;
+		$scope.gamesecond = 0;
+		$scope.gameminute= 0;
+		$scope.gameRight = 0;
+		$scope.gameWrong = 0;
+		$scope.choose = [-1,-1];
+		$scope.initCounter();
+		var totalElements = m*n / 2;
+		var elements = [
+			"bulbassauro.jpg",
+			"charmander.jpg",
+			"chikorita.png",
+			"crab.gif",
+			"cubone.png",
+			"hitmonchan.png",
+			"snorlax.png",
+			"squirtle.png",
+			"togepi.jpg",
+			"zoobat.png"
+		];
+		var usable = [];
+		for(var i=0; i < totalElements; i++){
+			usable.push(elements[i]);
+			usable.push(elements[i]);
+		}
+		$scope.game = [];
+		$scope.gameTurned = [];
+
+		for(var i = 0; i < m; i++){
+			$scope.game.push([]);
+			$scope.gameTurned.push([]);
+			for(var j = 0; j < n; j++) {
+				$scope.game[i][j] = usable.splice(Math.floor(Math.random()*usable.length),1)[0];
+				$scope.gameTurned[i][j] = true;
+			}
+		}
+
+		$timeout(function(){
+			for(var i = 0; i < m; i++){
+				for(var j = 0; j < n; j++) {
+					$scope.gameTurned[i][j]=false;
+				}
+			}
+		},m*n*500);
+
+	};
+
+	$scope.initialize($scope.currentLevel*2,$scope.currentLevel*2);
 
 	$scope.restartGame = function(){
-		console.log("as");
-		$window.location.reload();
+		$scope.currentLevel++;
+		$scope.initialize($scope.currentLevel*2,$scope.currentLevel*2);
 	}
+
+	$scope.setOption = function(line,colum){
+		if(!$scope.gameTurned[line][colum]){
+			if($scope.choose[0] == -1 ){
+				$scope.gameTurned[line][colum] = true;
+				$scope.choose[0] = line;
+				$scope.choose[1] = colum;
+			}else{
+				$scope.gameTurned[line][colum] = true;
+				$timeout(function() {
+					if($scope.game[$scope.choose[0]][$scope.choose[1]] != $scope.game[line][colum]){
+						$scope.gameTurned[$scope.choose[0]][$scope.choose[1]] = false;
+						$scope.gameTurned[line][colum] = false;
+						$scope.gameWrong++;
+
+					}else{
+						$scope.gameRight++;
+						if($scope.gameFull == $scope.gameRight){
+							$scope.gameWin = true;
+							$timeout.cancel($scope.timeProm);
+						}
+					}
+					$scope.choose[0] = -1;
+				}, 500);
+			}
+		}
+
+	}
+
+
+
 
 
 });
